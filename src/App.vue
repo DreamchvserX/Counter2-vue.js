9782ed39838cf4110c75e3e5bb6f40dc6788ab29
Justin Eigbefoh Oamen <104932091+DreamchvserX@users.noreply.github.com>
2023-09-02 19:16:37 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from "vue"
+const count = ref(0)
+
+const incCount = () => {
+  count.value = count.value + 1
+}
+
+const decCount = () => {
+  count.value = count.value - 1
+}
+</script>
+
+
+<template>
+  <main>
+    <div>
+      <h2>Current count is...</h2>
+      <h1>{{ count }}</h1>
+      <button @click="decCount()">-</button>
+      <button @click="incCount()">+</button>
+    </div>
+  </main>
+</template>
+
+
+<style scoped>
+  main{
+    background-color: gray;
+    display: flex;
+    height: 100vh;
+    width: 100vw;
+    justify-content: center;
+    align-items: center;
+  }
+
+  div{
+    text-align: center;
+  }
+</style>
